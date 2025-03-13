a34235dc72dc325f76279d23f73a25366158c1ee
rubithaselvam <rubitha3152004@gmail.com>
2025-03-13 12:22:12 +0530

    Add files via upload

---
+++ dictionary 2.py
@@ -0,0 +1,19 @@
+books = {
+    "The last city of dwarka": {"Author":"Shikaripur Ranganatha Rao (S.R. Rao)", "Establish year":1999,
+                                "About": "Hindhu mythology,KINGDOM OF KRISHNA"},
+    "7 Secrets of siva": {"Author":"Devdutt Pattanaik", "Establish year":2011,
+                          "About": "Myths and symbols surrounding the Hindu deity Shiva"}
+}
+
+print("books dictionary:", books)
+
+# creating a empty set
+empty_dict = {}
+print("empty dictionary:", empty_dict)
+
+# using dict() constructor
+author = dict(Author = "Ved Vyasa", Establish = 400, About = "the Pandavas and Kauravas")
+print("author dictionary:", author)
+
+# Accessing dictionary values
+print("author:", books["The last city of dwarka"])
