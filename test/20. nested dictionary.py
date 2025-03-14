2ae074c69a7a29803ac03a1474027375f8c446c2
Rubitha <rubitha3152004@gmail.com>
2025-03-14 18:07:14 +0530

    Add files via upload

---
+++ test/20. nested dictionary.py
@@ -0,0 +1,15 @@
+people = {
+   "person1" :{
+       "name" : "rubitha",
+       "age" : 22,
+       "hobby" : "watching movies"
+   },
+    "person2" : {
+        "name" : "manya",
+        "age" : 24,
+        "hobby" : "playing games"
+    }
+
+}
+print("person2 age:", people["person2"]["age"])
+print("person1 name:", people["person1"]["hobby"])
