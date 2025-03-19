d08d917437f5a142a640ab3ca7dd1b6f06d40ab4
Rubitha <rubitha3152004@gmail.com>
2025-03-19 13:55:26 +0530

    Update interest.py

--- test/functions/interest.py
+++ test/functions/interest.py
@@ -1,7 +1,7 @@
 def interest(p,r,t):
     return(p * r * t) / 100
 def amount(p,r,t):
-    return p + (p * r * t) / 100
+    return p + ((p * r * t) / 100)
 print(interest(10000,12,1))
 print(amount(10000,12,1))
 
