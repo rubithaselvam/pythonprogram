1fd64d8fb4774832aa55cad75130792d07c53929
Rubitha <rubitha3152004@gmail.com>
2025-04-05 12:30:35 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,6 @@
+class person:
+    def __init__(self,name = ""):
+        self.name = name # "name" its wrong after equal
+
+name1 = person("rubitha") # name its wrong before rubitha
+print(name1.name)
