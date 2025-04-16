6dfd62bf47a7bb2f6f33e239fbc17d062c3cf9cb
Rubitha <rubitha3152004@gmail.com>
2025-04-16 14:41:06 +0530

    Update multiple_inheritance.py

--- oops/multiple_inheritance.py
+++ oops/multiple_inheritance.py
@@ -19,4 +19,28 @@ rubitha.person2() # bracket is must here for all oops program
 rubitha.person1()
 rubitha.people()
 
+                         # ******* ANOTHER TYPE ********
 
+class friend1:
+    #name = "" # attributes
+    def __init__(self):
+        print("WELCOME FRIENDS")
+
+    def person1(self,name):
+        print(f"{name}: nice to see you guys")
+
+class friend2:
+    #name = "" # here it given or not given does not affect the program
+
+    def person2(self,name):
+        print(f"{name}:after a years we meted")
+
+class person(friend1,friend2):
+
+    def people(self,name):
+        print(f"{name}:I am happy to see you guys")
+
+rubitha = person() # which OBJECT name is given that name only give to tha below functions
+rubitha.person2("rubitha") # bracket is must here
+rubitha.person1("ramya")
+rubitha.people("udhaya")
